bd529e9e8a7adc93893188d290f0d0cc79ce976e
unknown <jamesstamper72@gmail.com>
2026-01-05 18:59:35 -0700

    swarm: establish multi-agent orchestration infrastructure

--- deploy.ps1
+++ deploy.ps1
@@ -3,17 +3,11 @@ param (
     [string]$Command
 )
 
-$Passwords = @{
-    1 = "RapidArchitecture2026TEN"
-    2 = "RapidArchitecture2026TEN"
-    3 = "RapidArchitecture2026TEN"
-}
-
 $IPs = @{
-    1 = "147.93.63.136"   # Worker 1: Build/Error Resolution
-    2 = "147.93.57.170"   # Worker 2: UX/UI and Website Builder
-    3 = "147.93.103.111"  # Worker 3: Infrastructure/Database
+    1 = "164.92.118.130"   # Worker 1: Build/Error Resolution
+    2 = "147.182.243.137"  # Worker 2: UX/UI and Website Builder
+    3 = "161.35.239.20"    # Worker 3: Infrastructure/Database
 }
 
-# This uses sshpass (standard on most systems) to send the password automatically
-sshpass -p $Passwords[$Worker] ssh root@$IPs[$Worker] "cd ~/worktree-1 && git pull origin dev && $Command"
+# Uses SSH key: ~/.ssh/ai_swarm_key (passwordless after setup)
+ssh -i "$env:USERPROFILE\.ssh\ai_swarm_key" root@$($IPs[$Worker]) "cd ~/worktree-1 && git pull origin dev && $Command"
